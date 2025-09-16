997c6b674ec0f79321c1d44a1a3e40328db2f011
andreaocean <amairambekova06@gmail.com>
2025-09-16 23:08:07 +0600

    HW4 added

--- main.py
+++ main.py
@@ -16,3 +16,12 @@
 
 
 from my_packege import minus, add, test
+
+
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
---
+++ mainHW.py
@@ -0,0 +1,25 @@
+# import cowsay
+# print(cowsay.cow("Hello!"))
+#
+# import cowsay
+#
+# def divide(a, b):
+#     if b == 0:
+#         print(cowsay.tux("Ошибка: деление на ноль!"))
+#     else:
+#         return a / b
+#
+# divide(10, 0)
+#
+# import cowsay
+#
+# def npc_say(text):
+#     print(cowsay.trex(text))
+#
+# npc_say("Я хранитель этого леса!")
+
+
+# import emoji
+
+# print(emoji.emojize("Our teacher is :fire: :fire:"))
+
